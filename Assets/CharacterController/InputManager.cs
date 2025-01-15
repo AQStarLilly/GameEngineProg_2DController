705c92c61d917eb82e3f17cb81ad7ccf5d90e23a
Kieran Hislop <kieranjhislop@gmail.com>
2025-01-15 15:16:01 -0400

    Scripts and extras all added

---
+++ Assets/CharacterController/InputManager.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using UnityEngine.InputSystem;
+
+public class InputManager : MonoBehaviour, GameInput.IPlayerActions
+{
+
+    private GameInput gameInput;
+
+    void Awake()
+    {
+        gameInput = new GameInput();
+        gameInput.Player.Enable();
+
+        gameInput.Player.SetCallbacks(this);
+    }
+
+
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        if (context.performed)
+        {
+            Debug.Log("Move input hit : " + context.ReadValue<Vector2>());
+
+            Actions.MoveEvent?.Invoke(context.ReadValue<Vector2>());
+        }        
+    }
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
+
+
+public static class Actions
+{
+    public static Action<Vector2> MoveEvent;
+}
---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public CharacterController characterController;
+
+    public Vector2 moveDirection = new Vector2(0.00f, 1.00f);
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        characterController = this.GetComponent<CharacterController>();
+    }  
+
+    void HandlePlayerMovement()
+    {
+        characterController.Move(moveDirection * Time.deltaTime);
+    }
+}
